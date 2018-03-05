d9bb7e7175a2de3fc79055249494b9e8d3e43d35
Holden Altaffer <haltaffer@gmail.com>
2018-03-05 00:46:06 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  attr_accessor :name
+  @@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+  def clear_all
+    @@all.clear
+  end
+
+  def all
+    @@all.each do |name|
+      puts name
+  end
+
+
+end
